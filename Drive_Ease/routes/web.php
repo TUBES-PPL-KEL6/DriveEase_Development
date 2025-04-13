419b99f8b530cbd65316bc3406f17a3102dc8118
james hestu satria <jamesleo164@gmail.com>
2025-04-13 12:14:03 +0700

    login-regist

--- Drive_Ease/routes/web.php
+++ Drive_Ease/routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/redirect', function () {
+    $role = auth()->user()->role;
+
+    if ($role === 'admin') {
+        return redirect('/admin/dashboard');
+    } elseif ($role === 'rental') {
+        return redirect('/rental/dashboard');
+    } else {
+        return redirect('/user/dashboard');
+    }
+})->middleware('auth')->name('dashboard.redirect');
+
+
+Route::middleware(['auth'])->group(function () {
+    // User
+    Route::get('/user/dashboard', fn() => view('dashboard.user'))->name('user.dashboard');
+
+    // Rental
+    Route::get('/rental/dashboard', fn() => view('dashboard.rental'))->name('rental.dashboard');
+
+    // Admin
+    Route::get('/admin/dashboard', fn() => view('dashboard.admin'))->name('admin.dashboard');
+});
+
+
+require __DIR__.'/auth.php';
