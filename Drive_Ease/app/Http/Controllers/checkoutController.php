28cac5903c6a792a8d4e294a4e70ea2e8ddb83b5
humamrazzan <humamraz@gmail.com>
2025-04-29 12:25:33 +0700

    Fitur Konfirmasi dan Notifikasi

--- Drive_Ease/app/Http/Controllers/checkoutController.php
+++ Drive_Ease/app/Http/Controllers/checkoutController.php
@@ -2,19 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class checkoutController extends Controller
 {
     public function checkout(Request $request)
     {
-        return view('payment.checkout');
-        return redirect()->route('user.dashboard');
+        $cars = Car::all();
+        return view('payment.checkout', compact('cars'));
     }
 
     public function returnToDashboard()
     {
         return redirect()->route('user.dashboard');
     }
-
 }
