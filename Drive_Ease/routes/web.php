45ca85e4e4382abdb37dbce02b5031543f476313
humamrazzan <humamraz@gmail.com>
2025-04-29 22:38:07 +0700

    SCRUM-18 PBI11 Konfirmasi & Notifikasi

--- Drive_Ease/routes/web.php
+++ Drive_Ease/routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\PaymentHistoryController;
 use App\Http\Controllers\checkoutController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RentController;
+use App\Http\Controllers\RentalRentController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('landing');
@@ -53,7 +55,25 @@ Route::middleware(['auth'])->group(function () {
     // Admin view payment_history
     Route::get('/payment-history', [PaymentHistoryController::class, 'index'])->middleware('auth')->name('payment.index');
 
+    // Pelanggan
+    Route::get('/user/rents', [RentController::class, 'index'])->name('rents.index');
+    Route::get('/user/rents/{id}', [RentController::class, 'show'])->name('rents.show');
+    Route::post('/user/rents', [RentController::class, 'store'])->name('rents.store');
+    Route::post('/user/rents/{id}/reject', [RentController::class, 'rejectRent'])->name('rents.reject');
+    Route::post('/user/rents/reConfirm', [RentController::class, 'reConfirm'])->name('rents.reConfirm');
+
+    // Rental
+    Route::get('/rental/rents', [RentalRentController::class, 'index'])->name('rental.rents.index');
+    Route::get('/rental/rents/{id}', [RentalRentController::class, 'show'])->name('rental.rents.show');
+    Route::post('/rental/rents/{id}/confirm', [RentalRentController::class, 'confirmRent'])->name('rental.rents.confirm');
+    Route::post('/rental/rents/{id}/reject', [RentalRentController::class, 'rejectRent'])->name('rental.rents.reject');
+    // Route::post('/rental/rents', [RentController::class, 'store'])->name('rents.store');
+
+    Route::get('/notifications/fetch', [NotificationController::class, 'fetchNotifications'])->name('notifications.fetch');
+    Route::get('/notifications/count', [NotificationController::class, 'countNotification'])->name('notifications.count');
+    Route::post('/notifications/store', [NotificationController::class, 'store'])->name('notifications.store');
+    Route::post('/notifications/markAsRead', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
 });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
