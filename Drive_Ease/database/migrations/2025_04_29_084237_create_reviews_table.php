f3cf294caf31aad3f6398d79770f719427e12328
james hestu satria <jamesleo164@gmail.com>
2025-05-02 21:58:35 +0700

    Update 2025_04_29_084237_create_reviews_table.php

--- Drive_Ease/database/migrations/2025_04_29_084237_create_reviews_table.php
+++ Drive_Ease/database/migrations/2025_04_29_084237_create_reviews_table.php
@@ -6,17 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateReviewsTable extends Migration
 {
-    public function up(): void
-    {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('car_id')->constrained()->onDelete('cascade');
-            $table->tinyInteger('rating'); // 1–5
-            $table->text('comment')->nullable();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('reviews', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->foreignId('vehicle_id')->constrained()->onDelete('cascade');
+        $table->tinyInteger('rating')->unsigned()->comment('1 to 5');
+        $table->text('comment')->nullable();
+        $table->timestamps();
+    });
+}
+
 
     public function down(): void
     {
