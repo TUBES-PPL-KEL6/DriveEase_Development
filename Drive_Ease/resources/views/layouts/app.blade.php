5ea67d50fbded567c6d43e71709f80343388d9e4
james hestu satria <jamesleo164@gmail.com>
2025-05-08 00:38:22 +0700

    Update app.blade.php

--- Drive_Ease/resources/views/layouts/app.blade.php
+++ Drive_Ease/resources/views/layouts/app.blade.php
@@ -53,9 +53,10 @@
         @endisset
 
         <!-- Content -->
-        <main class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            {{ $slot }}
-        </main>
+<main class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    @yield('content') {{-- ✅ Sesuai dengan penggunaan @section di view --}}
+</main>
+
 
     </div>
 
