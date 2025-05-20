edbeb3432783d788d842d47df5188cad5a411522
mchaz <mochammadaziiz02@gmail.com>
2025-05-20 22:26:18 +0700

    PBI #25 progress

--- Drive_Ease/resources/views/layouts/app.blade.php
+++ Drive_Ease/resources/views/layouts/app.blade.php
@@ -73,6 +73,7 @@
             }
         </script>
     @endauth
+    @livewireStyles
 </head>
 
 <body class="font-sans antialiased bg-gray-100">
@@ -139,6 +140,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
     @stack('scripts')
+    @livewireScripts
 </body>
 
 </html>
