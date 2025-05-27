bb15523c8387859e5f73c0762579d75c423849b6
humamrazzan <humamraz@gmail.com>
2025-05-27 23:48:14 +0700

    back to pbi 36

--- Drive_Ease/resources/views/bookings/mine.blade.php
+++ Drive_Ease/resources/views/bookings/mine.blade.php
@@ -1,18 +1,6 @@
 @extends('layouts.app')
 
-<x-slot name="header">
-    <h2 class="font-semibold text-xl leading-tight">
-        Dashboard Pelanggan
-    </h2>
-</x-slot>
-
-<div class="py-6">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-4">
-        <div class="p-6 rounded-md">
-            <div class="bg-dark text-white p-6 rounded-md">
-            
 @section('content')
-
     <div class="py-8 px-4 md:px-8">
         <div class="max-w-7xl mx-auto">
             <div class="flex justify-between items-center mb-6">
