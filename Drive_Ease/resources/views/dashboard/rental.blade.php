3d459642a6152473e404b9c2ba27eda067d7f4b5
MOCHAMMAD AZIIZ NUGROHO <133506527+mchaz@users.noreply.github.com>
2025-06-04 16:59:30 +0700

    Delete Drive_Ease directory

--- Drive_Ease/resources/views/dashboard/rental.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Dashboard Rental</h1>
-<p>Selamat datang, {{ auth()->user()->name }}</p>
-
-<form method="POST" action="{{ route('logout') }}" style="margin-top: 20px;">
-    @csrf
-    <button type="submit">Logout</button>
-</form>
